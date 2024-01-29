2473fb8322f8cbadfec34f62a6926a7ffe7f3ca6
JavdeMora <133394751+JavdeMora@users.noreply.github.com>
2024-01-29 09:35:34 +0100

    Update Photo-z_architecture.py

--- Photo-z_architecture.py
+++ Photo-z_architecture.py
@@ -1,7 +1,6 @@
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import numpy as np
 
 ##########################################################################################################

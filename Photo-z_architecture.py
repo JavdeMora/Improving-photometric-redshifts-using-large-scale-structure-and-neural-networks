dd2a52f190e4bfa0f1d516b8fa8f7f82237621e5
JavdeMora <133394751+JavdeMora@users.noreply.github.com>
2024-01-29 09:33:20 +0100

    Update and rename Script architecture photo-z.py to Photo-z_architecture.py

---
+++ Photo-z_architecture.py
@@ -0,0 +1,44 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+##########################################################################################################
+
+#Mixture Density Network
+
+##########################################################################################################
+class network(torch.nn.Module):
+    def __init__(self, nhidden, num_gauss):
+        super().__init__()
+        self.inputlay = torch.nn.Sequential(nn.Linear(6, 20),nn.LeakyReLU(0.1))
+        
+        params = np.linspace(20,200,nhidden)
+        modules = []
+        for k in range(nhidden-1):
+            modules.append(nn.Linear(int(params[k]) ,int(params[k+1])))
+            modules.append(nn.LeakyReLU(0.1))  
+        self.hiddenlay = nn.Sequential(*modules)
+        
+        
+        self.logalphas = torch.nn.Sequential(nn.Linear(200 ,100),nn.LeakyReLU(0.1),nn.Linear(100, num_gauss))                
+        self.means = torch.nn.Sequential(nn.Linear(200 ,100),nn.LeakyReLU(0.1),nn.Linear(100, num_gauss))
+        self.logstds = torch.nn.Sequential(nn.Linear(200 ,100),nn.LeakyReLU(0.1),nn.Linear(100 ,num_gauss))
+        
+    def forward(self, inp):
+        
+        x = self.inputlay(inp)
+        x = self.hiddenlay(x)
+        mu = self.means(x)
+        logsig = self.logstds(x)
+        logalpha=self.logalphas(x)
+        
+        logsig = torch.clamp(logsig,-5,5)
+        
+        
+        logalpha = logalpha - torch.logsumexp(logalpha,1)[:,None] 
+
+
+        
+        return logalpha, mu, logsig  

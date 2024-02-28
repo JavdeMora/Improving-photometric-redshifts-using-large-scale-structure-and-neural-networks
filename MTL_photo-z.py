e31c1871de08119988a5a8badc5bfe407bb3844d
Laura Cabayol <lauracabayol@gmail.com>
2024-02-28 10:45:15 +0100

    clean loader function

--- MTL_photo-z.py
+++ MTL_photo-z.py
@@ -124,81 +124,45 @@ class MTL_photoz:
             })   
         return colors_df
         
-        
-        
+    def _get_loaders_photoz(self, test_size, val_size, batch_size):
+        """
+        Split the data into train, validation, and test datasets and create data loaders.
 
-    def _get_colors(self):
-        
-        
-        
+        Args:
+            test_size (float): The proportion of the dataset to include in the test split.
+            val_size (float): The proportion of the training dataset to include in the validation split.
+            batch_size (int): The batch size for the data loaders.
 
-            
-        if 'i' in parquet.columns and 'g' in parquet.columns and 'r' in parquet.columns and 'z' in parquet.columns and 'h' in parquet.columns and 'j' in parquet.columns and 'y' in parquet.columns:
-            parquet_labeled=parquet
-        
-        else:
-            parquet_labeled=parquet.rename(columns={'euclid_vis_el_model3_ext_odonnell_ext':'vis','euclid_vis_el_model3_ext_odonnell_ext_error_realization':'err_vis','lsst_g_el_model3_ext_odonnell_ext':'g','lsst_i_el_model3_ext_odonnell_ext':'i','lsst_r_el_model3_ext_odonnell_ext':'r','lsst_z_el_model3_ext_odonnell_ext':'z','euclid_nisp_y_el_model3_ext_odonnell_ext':'y','euclid_nisp_j_el_model3_ext_odonnell_ext':'j','euclid_nisp_h_el_model3_ext':'h','lsst_g_el_model3_ext_odonnell_ext_error_realization':'err_g','lsst_i_el_model3_ext_odonnell_ext_error_realization':'err_i','lsst_r_el_model3_ext_odonnell_ext_error_realization':'err_r','lsst_z_el_model3_ext_odonnell_ext_error_realization':'err_z','euclid_nisp_y_el_model3_ext_odonnell_ext_error_realization':'err_y','euclid_nisp_j_el_model3_ext_odonnell_ext_error_realization':'err_j','euclid_nisp_h_el_model3_ext_odonnell_ext_error_realization':'err_h',})
-            parquet_labeled['i']=parquet_labeled['i']+parquet_labeled['err_i']
-            parquet_labeled['g']=parquet_labeled['g']+parquet_labeled['err_g']
-            parquet_labeled['r']=parquet_labeled['r']+parquet_labeled['err_r']
-            parquet_labeled['z']=parquet_labeled['z']+parquet_labeled['err_z']
-            parquet_labeled['h']=parquet_labeled['h']+parquet_labeled['err_h']
-            parquet_labeled['j']=parquet_labeled['j']+parquet_labeled['err_j']
-            parquet_labeled['y']=parquet_labeled['y']+parquet_labeled['err_y']
-            parquet_labeled=parquet_labeled.drop(['err_z','err_i','err_y','err_j','err_h','err_g','err_r','err_vis','dec_gal','ra_gal'], axis=1)
-        #Mags
-        parquet_labeled['i']= -2.5*np.log10(parquet_labeled['i'])-48.6
-        parquet_labeled['g']= -2.5*np.log10(parquet_labeled['g'])-48.6
-        parquet_labeled['r']= -2.5*np.log10(parquet_labeled['r'])-48.6
-        parquet_labeled['z']= -2.5*np.log10(parquet_labeled['z'])-48.6
-        parquet_labeled['h']= -2.5*np.log10(parquet_labeled['h'])-48.6
-        parquet_labeled['j']= -2.5*np.log10(parquet_labeled['j'])-48.6
-        parquet_labeled['y']= -2.5*np.log10(parquet_labeled['y'])-48.6
-        if 'vis' in parquet_labeled.columns:
-            parquet_labeled['vis']= -2.5*np.log10(parquet_labeled['vis'])-48.6
-        #Drop NaN
-        parquet_labeled=parquet_labeled.dropna(axis=0,how='any')
-        
-        #Filter data (Mag_i < 25, z<1)
-        filtered_parquet=parquet_labeled[parquet_labeled['i']< 25]
-        if 'observed_redshift_gal' in filtered_parquet.columns:
-            filtered_parquet=filtered_parquet[filtered_parquet['observed_redshift_gal']<1]
-        dataset = filtered_parquet
-        
-        
-        
-        #Create colour dataframe
-        if 'vis' in parquet_labeled.columns and 'observed_redshift_gal' in parquet_labeled.columns:
-            colors_df = pd.DataFrame(np.c_[dataset['observed_redshift_gal'],dataset['vis'],dataset['g']-dataset['r'],dataset['r']-dataset['i'],dataset['i']-dataset['z'], dataset['z']-dataset['y'], dataset['y']-dataset['j'],dataset['j']-dataset['h']], columns=['observed_redshift_gal','Mag_i','g-r','r-i','i-z','z-y','y-j','j-h'])
-        else:
-            colors_df = pd.DataFrame(np.c_[dataset['g']-dataset['r'],dataset['r']-dataset['i'],dataset['i']-dataset['z'], dataset['z']-dataset['y'], dataset['y']-dataset['j'],dataset['j']-dataset['h']], columns=['g-r','r-i','i-z','z-y','y-j','j-h'])
-        return colors_df
-        
-    def _get_loaders(self, test_size, val_size, batch_size,  *args):
-        #Split data to train, validation and test datasets
-        test_size= test_size
-        train_dataset, test_dataset=train_test_split(self.cat, test_size=test_size)
-        #Split data to train, validation and test datasets
-        val_size = val_size
-        train_df, val_df = train_test_split(train_dataset, test_size=val_size)
-        #Create Tensor datasets
-        input_labs = ['g-r','r-i','i-z','z-y','y-j','j-h']
-        target_lab = ['observed_redshift_gal']
-        train_input=torch.Tensor(train_df[input_labs].values)
-        val_input= torch.Tensor(val_df[input_labs].values)
-        train_target = torch.Tensor(train_df[target_lab].values)
-        val_target = torch.Tensor(val_df[target_lab].values)
-        i_test=test_dataset['Mag_i'].values
-        test_input=torch.Tensor(test_dataset[input_labs].values)
-        test_target=test_dataset[target_lab].values
-        train_dataset = TensorDataset(train_input,train_target)
-        val_dataset = TensorDataset(val_input,val_target)
-        #Create loaders
-        batch_size=batch_size
-        loader_train = DataLoader(train_dataset, batch_size = batch_size, shuffle=True)
-        loader_val = DataLoader(val_dataset, batch_size = batch_size, shuffle =False)
-        
-        return loader_train, loader_val
+        Returns:
+            DataLoader: Training data loader.
+            DataLoader: Validation data loader.
+            DataLoader: Test data loader.
+        """
+        # Split data into input (features) and target variables
+        input_cols = ['g-r', 'r-i', 'i-z', 'z-y', 'y-j', 'j-h']
+        target_col = 'observed_redshift_gal'
+
+        X = self.cat_colors[input_cols].values
+        y = self.cat_colors[target_col].values.reshape(-1, 1)  # Ensure target column has the correct shape
+
+        # Split data into train and test datasets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+        # Further split train dataset into train and validation datasets
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val_size)
+
+        # Convert data to PyTorch tensors
+        train_dataset = TensorDataset(torch.Tensor(X_train), torch.Tensor(y_train))
+        val_dataset = TensorDataset(torch.Tensor(X_val), torch.Tensor(y_val))
+        test_dataset = TensorDataset(torch.Tensor(X_test), torch.Tensor(y_test))
+
+        # Create data loaders
+        loader_train = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        loader_val = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+        loader_test = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+        return loader_train, loader_val, loader_test
+
 
     def train_photoz(self, test_size=0.2, val_size=0.25, *args): #argumento solo catalogo
         loader_train, loader_val = self._get_loaders(test_size, val_size, self.batch_size)

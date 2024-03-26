4fd37c5e2677521069171768baee002ac43e68bf
Javier de Mora <133394751+JavdeMora@users.noreply.github.com>
2024-03-26 23:45:48 +0100

    Update Tutorial_photoz.py
    delete arg descriptions

--- Notebooks/Tutorial_photoz.py
+++ Notebooks/Tutorial_photoz.py
@@ -14,7 +14,7 @@
 # ---
 
 # %% [markdown]
-# # TOTORIAL TO MAKE A REDSHIFT PREDICTION WITH A NEURAL NETWORK
+# # TUTORIAL TO MAKE A REDSHIFT PREDICTION WITH A NEURAL NETWORK
 
 # %% [markdown]
 # ### Import repo scripts
@@ -25,10 +25,10 @@ sys.path.append('../mtl_lss')
 from Photoz_method import photoz
 
 # %% [markdown]
-# ### Definde the model to train and make photo-z predictions
+# ### Define the model to train and make photo-z predictions
 
 # %%
-#Initialize the class providing required and optional argumentsaaaaa
+#Initialize the class
 Model = photoz(
     photoz_hlayers = 5,
     photoz_num_gauss = 5, 
@@ -42,37 +42,28 @@ Model = photoz(
 # %%
 #Train the model with the data provided
 Model.train_photoz(
-    
-    #Set by default arguments (if left blank)
-    test_size=0.2, #Subset size used for testing
-    val_size=0.25 #Subset size used for training validation
-    
+    test_size=0.2,
+    val_size=0.25 
 )
 
 # %%
 #Make redshift prediction with color input
 Model.pred_photoz(
-    
-    #Required arguments (to pick by user)
-    test_colors=[ 0.5304,  0.4784, -0.0526,  0.2745,  0.6954, -0.1624], #Input the array of colors
-    
-    #Set by default arguments (if left blank)
-    plot=True #Choose if you want to plot the redshift probability distribution
-    
+    #Input the array of colors
+    test_colors=[ 0.5304,  0.4784, -0.0526,  0.2745,  0.6954, -0.1624], 
+    plot=True 
 )
 
 # %%
+#Make redshift prediction for a set of colors
 Model.pred_photoz_arr(
-    
-    #Required arguments (to pick by user)
+    #Input the array of colors
     test_colors = [
         [ 0.5304,  0.4784, -0.0526,  0.2745,  0.6954, -0.1624],
         [ 1.0710,  0.4091,  0.3013,  0.3987,  0.3970,  0.2368],
         [ 0.0593,  0.0383,  0.0917, -0.1004,  0.0179, -0.1460]
-    ] , #Input the arrays of colors (more than 1)
-    
-    #Set by default arguments (if left blank)
-    plot=True #Choose if you want to plot the redshift probability distribution for each galaxy
+    ] ,
+    plot=True
 )
 
 # %%

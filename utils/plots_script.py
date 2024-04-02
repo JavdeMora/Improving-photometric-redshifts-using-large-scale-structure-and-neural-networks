831161e9c97d61bff9414761dbf6fb6ddb8b3c1f
Javier de Mora <133394751+JavdeMora@users.noreply.github.com>
2024-04-03 01:18:02 +0200

    Update plots_script.py
    added plot 2pcf: not tested

--- utils/plots_script.py
+++ utils/plots_script.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
+#Photo-z probability distribution plot
 def plot_redshift_distribution(df, alpha, mu, sigma):
     x = np.linspace(0, 1, 1000)
     for j in range(len(df)):
@@ -26,3 +27,34 @@ def plot_redshift_distribution(df, alpha, mu, sigma):
         mean_pdf_line = plt.axvline(df['z_mean'].values[j], color='g', linestyle='-', label='mean')
         plt.legend()
         plt.show()
+
+#2PCF curves comparison
+def plot_2PCF(pred_ratio, d, drand, min_sep, max_sep, nedges):
+    # Calling real data
+    distA = d.flatten()
+    distB = drand.flatten()
+    # Compute 2PCF using the real data
+    th = np.linspace(np.log10(min_sep), np.log10(max_sep), nedges)
+    thetac = 10**np.array([(th[i]+th[i+1])/2 for i in range(len(th)-1)])
+    theta = 10**th * (1./60.)
+    thetac = thetac/60
+    ratio_dists = np.array([len(distA[(distA>theta[k])&(distA<theta[k+1])]) / len(distB[(distB>theta[k])&(distB<theta[k+1])]) for k in range(len(theta)-1)])
+
+    network_color = 'crimson'
+    true_color = 'navy'
+    fig=plt.figure(figsize=(10, 6))
+    # Plot predicted 2PCF 
+    plt.errorbar(thetac_test, pred_ratio_jk.mean(0), pred_ratio_jk.std(0), color=network_color, ls='--', label='$p(DD)/p(RR)$ - Network', marker='.', markersize=8)
+
+    # Plot 2PCF conventional method
+    plt.plot(thetac, ratio_dists - 1, color=true_color, label='$p(DD)/p(RR)$ - True', linewidth=2, marker='o', markersize=8)
+
+    plt.xscale('log')
+    plt.ylabel(r'$w(\theta)$', fontsize=16)
+    plt.xlabel(r'$\theta$', fontsize=16)
+    plt.title('Comparison of Network Predictions and True Values', fontsize=18)
+    plt.grid(which='both', linestyle='--', alpha=0.7)
+    plt.legend()
+    plt.tick_params(axis='both', which='both', labelsize=12)
+    plt.show()
+
